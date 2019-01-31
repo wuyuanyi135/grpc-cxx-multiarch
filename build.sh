2cd3113871439fa9eb84734c8763cf74a4baaa21
wuyuanyi <wuyuanyi135@gmail.com>
2019-01-30 19:13:25 -0500

    updated

--- build.sh
+++ build.sh
@@ -7,18 +7,26 @@ case $i in
 
     shift # past argument=value
     ;;
+    -v=*|--version=*)
+    VERSION="${i#*=}"
+    shift
+    ;;
     *)
           # unknown option
     ;;
-esac
+esac 
 done
 
+if [[ -z $VERSION ]]; then
+	echo "Version not specified"
+	exit 1
+fi
 bash ./build_dockerfiles.sh
 
 if [[ ! -z $BUILD_SOURCE ]]; then
-	docker build . -f Dockerfile.source -t wuyuanyi/grpc-cxx:source
+	docker build . -f Dockerfile.source --rm --compress --squash -t wuyuanyi/grpc-cxx:${VERSION}-source -t wuyuanyi/grpc-cxx:latest-source --build-arg VERSION=${VERSION}
 else
     echo "Skip build source image"
 fi
-docker build . -f Dockerfile.amd64 -t wuyuanyi/grpc-cxx:amd64
-docker build . -f Dockerfile.arm64v8 -t wuyuanyi/grpc-cxx:arm64v8 
+docker build . -f Dockerfile.amd64 -t wuyuanyi/grpc-cxx:${VERSION}-amd64 -t wuyuanyi/grpc-cxx:latest-amd64 --compress --squash
+docker build . -f Dockerfile.arm64v8 -t wuyuanyi/grpc-cxx:${VERSION}-arm64v8 -t wuyuanyi/grpc-cxx:latest-arm64v8 --compress --squash
