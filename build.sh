0529e8d8efd02aa2ec68472665ec4fcee101a3c1
wuyuanyi <wuyuanyi135@gmail.com>
2019-02-02 01:55:59 -0500

    use cross compile

--- build.sh
+++ build.sh
@@ -21,12 +21,11 @@ if [[ -z $VERSION ]]; then
 	echo "Version not specified"
 	exit 1
 fi
-bash ./build_dockerfiles.sh
 
 if [[ ! -z $BUILD_SOURCE ]]; then
 	docker build . -f Dockerfile.source --rm --compress --squash -t wuyuanyi/grpc-cxx:${VERSION}-source -t wuyuanyi/grpc-cxx:latest-source --build-arg VERSION=${VERSION}
 else
     echo "Skip build source image"
 fi
-docker build . -f Dockerfile.amd64 -t wuyuanyi/grpc-cxx:${VERSION}-amd64 -t wuyuanyi/grpc-cxx:latest-amd64 --compress --squash
-docker build . -f Dockerfile.arm64v8 -t wuyuanyi/grpc-cxx:${VERSION}-arm64v8 -t wuyuanyi/grpc-cxx:latest-arm64v8 --compress --squash
+docker build . -f Dockerfile.cross.amd64 -t wuyuanyi/grpc-cxx:${VERSION}-amd64 -t wuyuanyi/grpc-cxx:latest-amd64 --compress --squash
+docker build . -f Dockerfile.cross.arm64 -t wuyuanyi/grpc-cxx:${VERSION}-arm64 -t wuyuanyi/grpc-cxx:latest-arm64 --compress --squash
--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker tag grpc-cxx:arm64v8 wuyuanyi/grpc-cxx:arm64v8
-docker tag grpc-cxx:amd64 wuyuanyi/grpc-cxx:amd64
-docker tag grpc-cxx:source wuyuanyi/grpc-cxx:source
-
-docker push wuyuanyi/grpc-cxx:arm64v8
-docker push wuyuanyi/grpc-cxx:amd64
-docker push wuyuanyi/grpc-cxx:source
